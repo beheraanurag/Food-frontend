c0409aab7cb9fba8e0b51e664a083b0fd0361fca
Demo-work <beheraanurag6@gmail.com>
2026-07-09 20:44:04 +0530

    contact page name update

--- src/components/Contactpage.jsx
+++ src/components/Contactpage.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import axios from "axios";
-import "./ContactPage.css";
+import "./Contactpage.css";
 
 function ContactPage() {
   const [formData, setFormData] = useState({
